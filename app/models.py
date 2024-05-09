402173b9630e7ee2dbe87aa0a13d4252afd748c2
Sharon Mwende <sharonndisya2002@gmail.com>
2024-05-09 23:40:53 +0300

    modified app.py and models.py

--- app/models.py
+++ app/models.py
@@ -3,41 +3,9 @@ from sqlalchemy_serializer import SerializerMixin
 from sqlalchemy.orm import validates, relationship
 from sqlalchemy.ext.associationproxy import association_proxy
 import re
+from sqlalchemy import CheckConstraint
 
 db = SQLAlchemy()
 
 # Define models
 
-# User model
-class User(db.Model):
-    pass
-
-
-
-# Product model
-class Product(db.Model):
-    pass
-
-
-
-
-# Order model
-class Order(db.Model):
-    pass
-
-
-
-# Analytics model
-class Order(db.Model):
-    pass
-
-
-
-
-
-# Category model
-class Category(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100), nullable=False, unique=True)
-    # Define relationship: a category can have many products
-    products = db.relationship('Product', backref='category', lazy=True)
