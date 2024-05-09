d5b649999378188e3984131f294134868d4d1c18
[james-mbuvi] <[james.mbuvi@student.moringaschool.com]>
2024-05-09 11:19:45 +0300

    Updated Authentication

--- app/app.py
+++ app/app.py
@@ -1,36 +1,35 @@
-from flask import Flask, request, jsonify, session
+from flask import Flask, request, jsonify
 from flask_bcrypt import Bcrypt
-from flask_session import Session
+from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity
 from config import ApplicationConfig
 from flask_cors import CORS
-from models import User,db
+from models import User, db
 
 app = Flask(__name__)
 CORS(app, supports_credentials=True)
 bcrypt = Bcrypt(app)
+app.config.from_object(ApplicationConfig)
+app.config['JWT_SECRET_KEY'] = 'ce2f33f294'
+jwt = JWTManager(app)
+db.init_app(app)
 
 @app.route("/signup", methods=["POST"])
 def register_user():
     email = request.json["email"]
     password = request.json["password"]
 
-
     user_exists = User.query.filter_by(email=email).first() is not None
 
     if user_exists:
-    
-     return jsonify({"error": f"User with email {email} already exists"}), 409
+        return jsonify({"error": f"User with email {email} already exists"}), 409
 
-    hashed_password = bcrypt.generate_password_hash(password)
+    hashed_password = bcrypt.generate_password_hash(password).decode('utf-8')
     new_user = User(email=email, password=hashed_password)
     db.session.add(new_user)
     db.session.commit()
-    
-    session["user_id"] = new_user.id
-    return jsonify({
-        "id": new_user.id,
-        "email": new_user.email
-    })
+
+    access_token = create_access_token(identity=email)
+    return jsonify({"access_token": access_token}), 200
 
 @app.route("/login", methods=["POST"])
 def login_user():
@@ -40,29 +39,17 @@ def login_user():
     user = User.query.filter_by(email=email).first()
 
     if user is None:
-        return jsonify({"error": "wrong email"}), 401
+        return jsonify({"error": "User not found"}), 401
     if not bcrypt.check_password_hash(user.password, password):
         return jsonify({"error": "Password is incorrect"}), 401
     
-    session["user_id"] = user.id
-    return jsonify({
-        "id": user.id,
-        "email": user.email
-    })
+    access_token = create_access_token(identity=email)
+    return jsonify({"access_token": access_token}), 200
 
 @app.route("/logout", methods=["POST"])
+@jwt_required()
 def logout_user():
-    session.pop("user_id")
-    return "200"
+    return jsonify({"message": "Logged out successfully"}), 200
 
-app.config.from_object(ApplicationConfig)
-db.init_app(app)
-with app.app_context():
-  db.create_all()
-
-  
-
-
-
-  if __name__ == '__main__':
+if __name__ == '__main__':
     app.run(port=5555, debug=True)
