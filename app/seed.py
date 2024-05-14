5ced9e3a8df2f10713e49ea3427bac320d82aa31
ngigi1234 <francis.ngigi1@student.moringaschool.com>
2024-05-14 17:06:57 +0300

    contact model added

--- app/seed.py
+++ app/seed.py
@@ -1,35 +1,24 @@
-# seed.py
-# seed.py
-
 from app import app, db
 from models import User, Product, Order, Category, Contact
 from flask_bcrypt import Bcrypt
 
-    # seed.py
-
-from app import app, db
-from models import User, Product, Order, Category, Contact
-from flask_bcrypt import Bcrypt,app
-from models import Admin, Customer
+bcrypt = Bcrypt(app)
 
 def seed_database():
-    bcrypt = Bcrypt()
+    #Delete existing data
+    Contact.query.delete()
 
-    with app.app_context():
-        db.create_all()
-
-        # Create instances of Contact
-        contacts = [
-            Contact(name='John Doe', email='john@example.com', message='Hello, world!'),
-            Contact(name='Jane Doe', email='jane@example.com', message='Nice to meet you!')
-        ]
-        db.session.add_all(contacts)
-        db.session.commit()
+    # Create instances of Contact
+    contacts = [
+        Contact(name='John Doe', email='john@example.com', message='Hello, world!'),
+        Contact(name='Jane Doe', email='jane@example.com', message='Nice to meet you!')
+    ]
+    db.session.add_all(contacts)
+    db.session.commit()
 
     # Create sample users
     hashed_password = bcrypt.generate_password_hash('password').decode('utf-8')
     admin_user = User(email='admin@example.com', password=hashed_password, role='admin')
-    
     regular_user = User(email='user@example.com', password=hashed_password, role='user')
 
     # Create sample categories
@@ -63,23 +52,6 @@ def seed_database():
     db.session.add(order2)
     db.session.commit()
 
-
-    # Seed Admins
-    hashed_password = bcrypt.generate_password_hash('adminpassword').decode('utf-8')
-    admin1 = Admin(username='admin1', password=hashed_password, role='admin')
-    admin2 = Admin(username='admin2', password=hashed_password, role='admin')
-    db.session.add(admin1)
-    db.session.add(admin2)
-
-    # Seed Customers
-    hashed_password = bcrypt.generate_password_hash('customerpassword').decode('utf-8')
-    customer1 = Customer(username='customer1', password=hashed_password, role='customer')
-    customer2 = Customer(username='customer2', password=hashed_password, role='customer')
-    db.session.add(customer1)
-    db.session.add(customer2)
-
-    db.session.commit()
-
-
 if __name__ == '__main__':
-    seed_database()
+    with app.app_context():
+        seed_database()
