9f13aad98d088f4d506ad040e38653704b412f89
Anna Kioko <annakioko00@gmail.com>
2024-05-14 12:03:38 +0300

    product model added

--- app/models.py
+++ app/models.py
@@ -39,3 +39,48 @@ class ProductAnalytics(db.Model, SerializerMixin):
 
     def _repr_(self):
         return f"<PrductAnalytics{self.id}, {self.product_id}, {self.total_sales}>"
+    
+    
+
+class Product(db.Model, SerializerMixin):
+    __tablename__ = 'products'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    gender = db.Column(db.String, CheckConstraint("gender IN ('Male', 'Female')"))
+    description = db.Column(db.String, nullable=False)
+    price = db.Column(db.Numeric(10, 2), nullable=False)
+    quantity_available = db.Column(db.Integer)
+    image = db.Column(db.String, nullable=True)
+
+    def serialize(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'gender': self.gender,
+            'description': self.description,
+            'price': str(self.price),  
+            'quantity_available': self.quantity_available,
+            'image': self.image,
+            'category': self.category.serialize() if self.category else None
+        }
+    
+
+    # relationship with category 
+    category_id = db.Column(db.Integer, db.ForeignKey('categories.id'))
+    category = db.relationship('Category', back_populates='products_list')
+
+    #relationship to order item 
+    order_items = db.relationship('OrderItem', back_populates='product', lazy=True)
+
+    #  relationship with ProductAnalytics model
+    analytics = db.relationship('ProductAnalytics', back_populates='product')
+
+    @validates('description')
+    def validate_description(self, key, description):
+        if not 5 <= len(description) <= 100:
+           raise ValueError("Description must be between 5 and 100 characters.")
+        return description
+    
+    def __repr__(self):
+        return f"<Product {self.id}, {self.name}, {self.gender}, {self.category}, {self.description}, {self.price}>"
