e098639591681b3628b7e6e4eccc514574423696
Sharon Mwende <sharonndisya2002@gmail.com>
2024-05-14 16:52:36 +0300

    deleted product analysis

--- app/models.py
+++ app/models.py
@@ -19,28 +19,6 @@ class User(db.Model, SerializerMixin):
         return f"User('{self.email}', '{self.role}')"
     
 
-class ProductAnalytics(db.Model, SerializerMixin):
-    _tablename_ = 'product_analytics'
-
-    id = db.Column(db.Integer, primary_key=True)
-    product_id = db.Column(db.Integer, db.ForeignKey('products.id'), nullable=False)
-    total_sales = db.Column(db.Integer, nullable=False, default=0)
-
-    
-    # Define relationship with Product model
-    product = db.relationship('Product', back_populates='analytics')
-
-    def serialize(self):
-        return {
-            'id': self.id,
-            'product_id': self.product_id,
-            'total_sales': self.total_sales
-        }
-
-    def _repr_(self):
-    
-    
-
 class Product(db.Model, SerializerMixin):
     __tablename__ = 'products'
 
@@ -86,23 +64,3 @@ class Product(db.Model, SerializerMixin):
 
     
 
-class ProductAnalytics(db.Model, SerializerMixin):
-    _tablename_ = 'product_analytics'
-
-    id = db.Column(db.Integer, primary_key=True)
-    product_id = db.Column(db.Integer, db.ForeignKey('products.id'), nullable=False)
-    total_sales = db.Column(db.Integer, nullable=False, default=0)
-
-    
-    # Define relationship with Product model
-    product = db.relationship('Product', back_populates='analytics')
-
-    def serialize(self):
-        return {
-            'id': self.id,
-            'product_id': self.product_id,
-            'total_sales': self.total_sales
-        }
-
-    def _repr_(self):
-        return f"<PrductAnalytics{self.id}, {self.product_id}, {self.total_sales}>"
