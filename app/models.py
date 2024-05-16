d225aada8e27cc051a21075a42b2980a43437e26
Annakioko <152261074+annakioko@users.noreply.github.com>
2024-05-16 20:00:43 +0300

    Feature/productmodel (#37)
    * product model added
    
    * refactored order model
    
    * refarctored models
    
    * migrations done
    
    * refactored signup route
    
    * added jwt authentication to user routes
    
    * authentication for order routes
    
    * product route authentication added
    
    * jwt added to categories
    
    * payment route defined
    
    * orderitems routes refactored
    
    * seed data

--- app/models.py
+++ app/models.py
@@ -1,27 +1,33 @@
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy_serializer import SerializerMixin
-from sqlalchemy.orm import validates, relationship
-from sqlalchemy.ext.associationproxy import association_proxy
+from sqlalchemy.orm import validates
 import re
+from enum import Enum
 from sqlalchemy import CheckConstraint
 
 db = SQLAlchemy()
 
-#Define model
+
+#define model
+
+#user model
 class User(db.Model, SerializerMixin):
     __tablename__ = 'users'
+
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String, nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(60), nullable=False)
-    role = db.Column(db.String(20), nullable=False)
+    username = db.Column(db.String, nullable=False,unique=True )
+    email = db.Column(db.String, nullable=False)
+    password = db.Column(db.String, nullable=False)
+    role = db.Column(db.String, nullable=False)
+    department = db.Column(db.String, nullable=True, default='N/A')
     address = db.Column(db.String, nullable=True, default='N/A' )
 
-        #relationship with order model
+    #relationship with order model
     orders = db.relationship('Order', back_populates='customer', lazy=True)
 
-        # Define one-to-many relationship with Cart
-    cart = db.relationship('Cart', back_populates='user', lazy=True)
+        # Define the one-to-many relationship with Contact
+    #contacts = db.relationship('Contact', back_populates='user', lazy=True)
+
 
     @validates('role')
     def validate_role(self, key, role):
@@ -51,89 +57,29 @@ class User(db.Model, SerializerMixin):
         return f"<User {self.id}, {self.username}, {self.email}, {self.password}, {self.role}, {self.address}>"
 
 
-class Order(db.Model, SerializerMixin):
-    id = db.Column(db.Integer, primary_key=True)
-    timestamp = db.Column(db.Date, nullable=False)
-    payment_status = db.Column(db.String(50), nullable=False)
-    shipping_address = db.Column(db.String(255), nullable=True, default='N/A')
-    payment_method = db.Column(db.String(50), nullable=False)
-    total_cost = db.Column(db.Float, nullable=False, default=0)
-    
-        #relationship with user
-    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
-    customer = db.relationship('User', back_populates='orders')
-
-     #relationship with orderitem
-    order_product = db.relationship('OrderItem', back_populates='order', lazy=True)
-      
-      #relationship with payment model
-    payment = db.relationship('Payment', back_populates='order', uselist=False)
-
-    # def serialize(self):
-    #     return {
-    #         'id': self.id,
-    #         'order_date': self.order_date.strftime('%Y-%m-%d %H:%M:%S'),
-    #         'total_amount': self.total_amount,
-    #         'payment_status': self.payment_status,
-    #         'shipping_address': self.shipping_address,
-    #         'payment_method': self.payment_method,
-    #         'user_id': self.user_id
-    #     }
-    
-    def __repr__(self):
-        return f"<Order {self.id}, {self.order_date}, {self.total_amount}, {self.payment_status}, {self.shipping_address}, {self.payment_method}>"
-
-    
-class OrderProduct(db.Model, SerializerMixin):
-    id = db.Column(db.Integer, primary_key=True)
-    order_id = db.Column(db.Integer, db.ForeignKey('orders.id'), nullable=False)
-    product_id = db.Column(db.Integer, db.ForeignKey('products.id'), nullable=False)
-    price = db.Column(db.Float, nullable=False)
-    quantity = db.Column(db.Integer, nullable=False)
-
-        # relationships with Order and Product models
-    order = db.relationship('Order', back_populates='order_product')
-    product = db.relationship('Product', back_populates='order_product', lazy=True)
-    cart_id = db.Column(db.Integer, db.ForeignKey('carts.id'), nullable=False)
-
-        # Define relationship with Cart model
-    cart = db.relationship('Cart', back_populates='order_product')
-
-    # def serialize(self):
-    #     return {
-    #         'id': self.id,
-    #         'order_id': self.order_id,
-    #         'product_id': self.product_id,
-    #         'quantity': self.quantity,
-    #         'price': self.price
-    #     }
-    
-
-    def __repr__(self):
-        return f"<OrderItem {self.id}, {self.quantity}, {self.price}>"
-    
+#Product model
 class Product(db.Model, SerializerMixin):
     __tablename__ = 'products'
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
-    gender = db.Column(db.String, CheckConstraint("gender IN ('Male', 'Female')"))
+    gender = db.Column(db.String)
     description = db.Column(db.String, nullable=False)
     price = db.Column(db.Numeric(10, 2), nullable=False)
     quantity_available = db.Column(db.Integer)
     image = db.Column(db.String, nullable=True)
 
-    # def serialize(self):
-    #     return {
-    #         'id': self.id,
-    #         'name': self.name,
-    #         'gender': self.gender,
-    #         'description': self.description,
-    #         'price': str(self.price),  
-    #         'quantity_available': self.quantity_available,
-    #         'image': self.image,
-    #         'category': self.category.serialize() if self.category else None
-    #     }
+    def serialize(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'gender': self.gender,
+            'description': self.description,
+            'price': str(self.price),  
+            'quantity_available': self.quantity_available,
+            'image': self.image,
+            'category': self.category.serialize() if self.category else None
+        }
     
 
     # relationship with category 
@@ -141,10 +87,15 @@ class Product(db.Model, SerializerMixin):
     category = db.relationship('Category', back_populates='products_list')
 
     #relationship to order item 
-    order_product = db.relationship('OrderItem', back_populates='product', lazy=True)
+    order_items = db.relationship('OrderItem', back_populates='product', lazy=True)
 
-    #  relationship with ProductAnalytics model
-    analytics = db.relationship('ProductAnalytics', back_populates='product')
+    @validates('gender')
+    def validate_gender(self, key, gender):
+       if gender not in ('Man', 'Woman'):
+           raise ValueError("Gender must be 'Man' or 'Woman'.")
+       return gender
+
+
 
     @validates('description')
     def validate_description(self, key, description):
@@ -155,8 +106,55 @@ class Product(db.Model, SerializerMixin):
     def __repr__(self):
         return f"<Product {self.id}, {self.name}, {self.gender}, {self.category}, {self.description}, {self.price}>"
     
+
+    # order model
+class Order(db.Model, SerializerMixin):
+    __tablename__ = 'orders'
+
+    id = db.Column(db.Integer, primary_key=True)
+    timestamp = db.Column(db.Date, nullable=False)
+    total_amount = db.Column(db.Float, nullable=False)
+    payment_status = db.Column(db.String(50), nullable=False)
+    shipping_address = db.Column(db.String(255), nullable=True, default='N/A')
+    payment_method = db.Column(db.String(50), nullable=False)
+
+    #relationship with user
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    customer = db.relationship('User', back_populates='orders')
+
+     #relationship with orderitem
+    order_items = db.relationship('OrderItem', back_populates='order', lazy=True)
+      
+      #relationship with payment model
+    payment = db.relationship('Payment', back_populates='order', uselist=False)
+
+
+    def __repr__(self):
+        return f"<Order {self.id}, {self.order_date}, {self.total_amount}, {self.payment_status}, {self.shipping_address}, {self.payment_method}>"
+
+
+#order item
+class OrderItem(db.Model, SerializerMixin):
+    __tablename__ = 'orderitems'
+
+    id = db.Column(db.Integer, primary_key=True)
+    order_id = db.Column(db.Integer, db.ForeignKey('orders.id'), nullable=False)
+    product_id = db.Column(db.Integer, db.ForeignKey('products.id'), nullable=False)
+    quantity = db.Column(db.Integer, nullable=False)
+    price = db.Column(db.Float, nullable=False)
+
+    # relationships with Order and Product models
+    order = db.relationship('Order', back_populates='order_items')
+    product = db.relationship('Product', back_populates='order_items', lazy=True)
+
+
+    def __repr__(self):
+        return f"<OrderItem {self.id}, {self.quantity}, {self.price}>"
+
+
+#Category Model
 class Category(db.Model, SerializerMixin):
-    _tablename_ = 'categories'
+    __tablename__ = 'categories'
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False, unique=True)
@@ -164,28 +162,12 @@ class Category(db.Model, SerializerMixin):
     # relationship with product
     products_list = db.relationship('Product', back_populates='category', lazy=True)
 
-    # def serialize(self):
-    #     return {
-    #         'id': self.id,
-    #         'name': self.name,
-           
-    #     }
-
-    def _repr_(self):
-        return f"<Category {self.id}, {self.name}>"
-    
-class Contact(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100), nullable=False)
-    email = db.Column(db.String(100), nullable=False)
-    message = db.Column(db.Text, nullable=False)
-
     def __repr__(self):
-        return f"Contact(name={self.name}, email={self.email})"
-    
+        return f"<Category {self.id}, {self.name}>"
+
 
 #payment model
-class Payment(db.Model):
+class Payment(db.Model, SerializerMixin):
     __tablename__ = 'payments'
 
     id = db.Column(db.Integer, primary_key=True)
@@ -198,19 +180,10 @@ class Payment(db.Model):
     # Define relationship with Order model
     order_id = db.Column(db.Integer, db.ForeignKey('orders.id'), nullable=False)
     order = db.relationship('Order', back_populates='payment')
-
-    def serialize(self):
-        return {
-            'id': self.id,
-            'amount': self.amount,
-            'method': self.method,
-            'status': self.status,
-            'order_id': self.order_id
-        }
     
     @validates(method)
     def validate_method(self, key, method):
-        if method not in ['card', 'mpesa', 'cash_on_pickup']:
+        if method not in ['card', 'mpesa']:
             raise ValueError("Invalid payment method.")
         return method
 
@@ -223,3 +196,30 @@ class Payment(db.Model):
 
     def __repr__(self):
         return f"<Payment {self.id}, {self.amount}, {self.method}, {self.status}>"
+    
+# class Contact(db.Model, SerializerMixin):
+#     __tablename__ ='contacts'
+#     id = db.Column(db.Integer, primary_key=True)
+#     name = db.Column(db.String(100), nullable=False)
+#     email = db.Column(db.String(100), nullable=False)
+#     message = db.Column(db.Text, nullable=False)
+#     contacts = db.relationship('Contact', back_populates='user', lazy=True)
+
+#         # Define the many-to-one relationship with User
+#     user = db.relationship('User', back_populates='contacts')
+
+
+#     def _repr_(self):
+#         return f"Contact(name={self.name}, email={self.email})"
+    
+    
+    
+
+    #issues
+    # unable to run migrations
+    #data is not relecting back at all
+    #order items delete and patch is not working at all
+    # not able to seed data 
+    # post operations are not reflecting in the db
+
+  
